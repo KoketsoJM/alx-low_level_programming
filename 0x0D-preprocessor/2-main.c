05211f17572963d415061defa1d1656b1d114798
KoketsoJM <Koketsomonchwe45@gmail.com>
2023-04-04 08:08:50 -0700

    project 0x0D.C

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+ /** 
+ * main - prints the name of the file it was compiled from
+ *
+ * return: 0 success
+ */
+int main(void)
+{
+	function("%s\n", _FILE_);
+	return (0)
+}
+
+
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+Project 0X0D.C
